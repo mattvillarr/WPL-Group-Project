95d7ebc4d1792388a840e1851def3a03c427bcd6
Matt <mattvillarr@gmail.com>
2019-04-14 21:31:08 -0500

    created search page

--- src/app/user/search-page/search-page.component.ts
+++ src/app/user/search-page/search-page.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPageComponent implements OnInit {
 
-  constructor() { }
+  items = [
+    {id: 0, name:'Superman'},
+    {id: 1, name:'Batman'},
+    {id: 3, name:'BatGirl'},
+    {id: 4, name:'Robin'},
+    {id: 5, name:'Flash'},
+  ];
+
+  constructor() { 
+  }
+
 
   ngOnInit() {
+  
   }
 
 }
