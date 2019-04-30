b870305f9cdf3835bbd4c0d1337d9e73f58194b1
Neha Aggarwal <neha@cometnet-10-21-37-165.utdallas.edu>
2019-04-30 12:25:01 -0500

    purchase history

--- src/app/user/profile-page/profile-page.component.ts
+++ src/app/user/profile-page/profile-page.component.ts
@@ -17,11 +17,22 @@ export class ProfilePageComponent implements OnInit {
 
   ngOnInit() {
     this.uid = localStorage.getItem('uid');
-    this.http.post("http://localhost:2345/orders/find", this.uid)
+    this.http.post("http://localhost:2345/orders/find", {userId: this.uid})
     .subscribe(response => {
-      this.items = response;
-      console.log(response);
-    });
+      console.log("Got response 1")
+      console.log("Looping")
+      let that = this
+      this.items = []
+      response['orders'].forEach(function(x) {
+        console.log("in loop 1")
+        console.log(x)
+        that.http.get("http://localhost:2345/products/" + x['product'])
+        .subscribe(response => {
+          console.log(response);
+          that.items.push(response);
+          console.log(that.items)
+        });
+      })
+    })
   }
-
 }
